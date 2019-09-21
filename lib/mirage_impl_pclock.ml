44ab0f56e728e99f93b82770d5d1e03604859636
Anil Madhavapeddy <anil@recoil.org>
2019-09-21 16:17:43 +0100

    do not pin mirage-clock-freestanding

--- lib/mirage_impl_pclock.ml
+++ lib/mirage_impl_pclock.ml
@@ -11,7 +11,7 @@ let posix_clock_conf = object
   method! packages =
     Mirage_key.(if_ is_unix)
       [ package ~min:"2.0.1" ~max:"3.0.0" "mirage-clock-unix" ]
-      [ package ~pin:"git+https://github.com/mirage/mirage-clock.git" "mirage-clock-freestanding" ]
+      [ package "mirage-clock-freestanding" ]
   method! connect _ modname _args = Fmt.strf "%s.connect ()" modname
 end
 
